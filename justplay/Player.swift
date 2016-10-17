dd2131703c87234737a442fb0533b2cbe540d33c
Htet Naing Aung <justin.aung19@gmail.com>
2016-10-18 00:40:02 +0700

    initial project commit with basic music play

---
+++ justplay/Player.swift
@@ -0,0 +1,39 @@
+//
+//  Player.swift
+//  justplay
+//
+//  Created by Justin Naing on 10/14/16.
+//  Copyright © 2016 JIT. All rights reserved.
+//
+
+import Foundation
+import MediaPlayer
+
+class Player
+{
+   var avPlayer: AVPlayer!
+   
+   init () {
+      avPlayer = AVPlayer()
+   }
+   
+   func playStream (fileUrl: String) {
+      let url = NSURL(string: fileUrl)
+      
+      avPlayer = AVPlayer(URL: url!)
+      avPlayer.play()
+      print("Playing stream")
+   }
+   
+   func playAudio () {
+      if (avPlayer.rate == 0 && avPlayer.error == nil) {
+         avPlayer.play()
+      }
+   }
+   
+   func pauseAudio () {
+      if (avPlayer.rate == 0 && avPlayer.error == nil) {
+         avPlayer.pause()
+      }
+   }
+}
---
+++ justplay/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  justplay
+//
+//  Created by Justin Naing on 10/13/16.
+//  Copyright © 2016 JIT. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class ViewController: UIViewController {
+
+   var player: Player!
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+      player = Player()
+      
+      let url = "http://htetnainga.sg-host.com/music_app/dubstep.mp3"
+      
+      player.playStream(url)
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
